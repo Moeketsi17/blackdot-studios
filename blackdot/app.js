a669128b606354ca7f84e1b54079ca3120fbad9b
Moeketsi17 <89091903+Moeketsi17@users.noreply.github.com>
2022-01-25 13:15:15 +0200

    Add files via upload

---
+++ blackdot/app.js
@@ -0,0 +1,15 @@
+const navLinks = document.querySelector(".nav-links")
+const burger = document.querySelector(".burger")
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle("active");
+
+    
+})
+
+
+
+
+
+
+
